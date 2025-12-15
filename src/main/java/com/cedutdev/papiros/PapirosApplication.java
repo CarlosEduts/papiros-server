6e5add5884b25150883c708d12bb4733dcc32c69
Carlos Silva <cedut.dev@gmail.com>
2025-12-15 19:34:20 -0300

    chore(init): Inicializa o repositório e a estrutura base do projeto

---
+++ src/main/java/com/cedutdev/papiros/PapirosApplication.java
@@ -0,0 +1,13 @@
+package com.cedutdev.papiros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PapirosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PapirosApplication.class, args);
+    }
+
+}
