a666d59ca529c486c5ccf05cafa5c9d1f8cf607a
Carlos Silva <cedut.dev@gmail.com>
2026-01-08 13:42:02 -0300

    docs(auth): adiciona documentação OpenAPI/Swagger no AuthenticationController

--- src/main/java/com/cedutdev/papiros/controller/AuthenticationController.java
+++ src/main/java/com/cedutdev/papiros/controller/AuthenticationController.java
@@ -4,6 +4,10 @@ import com.cedutdev.papiros.dto.LoginDTO;
 import com.cedutdev.papiros.dto.RegisterDTO;
 import com.cedutdev.papiros.dto.TokenDTO;
 import com.cedutdev.papiros.service.AuthenticationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -16,15 +20,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("auth")
 @RequiredArgsConstructor
+@Tag(name = "Autenticação", description = "Gerenciamento de autenticação de usuários")
 public class AuthenticationController {
 
     private final AuthenticationService authenticationService;
 
+    @Operation(summary = "Entrar com um usuário", description = "Este endpoint permite o login de usuários já registrados e retorna o Token")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200")
+    })
     @PostMapping("/login")
     public ResponseEntity<TokenDTO> login(@RequestBody @Valid LoginDTO data) {
         return ResponseEntity.ok(authenticationService.authenticateAndGenerateToken(data));
     }
 
+    @Operation(summary = "Cadastrar um novo usuário", description = "Este endpoint permite registrar um novo usuário")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "400"),
+            @ApiResponse(responseCode = "201")
+    })
     @PostMapping("/register")
     public ResponseEntity<Void> register(@RequestBody @Valid RegisterDTO data) {
         boolean registerSuccess = authenticationService.registerNewUser(data);
